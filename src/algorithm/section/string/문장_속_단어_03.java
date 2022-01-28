3ebbde2ff8722e1fb1d8a303e81854bdc77c948f
kimmin <kimmin2301@gmail.com>
2022-01-29 00:07:53 +0900

    Add solution 문장 속 단어 01-03

--- src/algorithm/section/string/문장_속_단어_03.java
+++ src/algorithm/section/string/문장_속_단어_03.java
@@ -1,6 +1,8 @@
+package algorithm.section.string;
+
 import java.util.Scanner;
 
-public class Main {
+public class 문장_속_단어_03 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

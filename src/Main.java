9361e7587f0a6d88326c8a486f5c661565ab211a
kimmin <kimmin2301@gmail.com>
2022-02-03 21:46:27 +0900

    Add solution 소수 에라토스테네스 체 02-05

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,21 +6,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
-        int[] answer = solution(n);
-        for (int i : answer) {
-            System.out.print(i + " ");
-        }
+        int answer = solution(n);
+        System.out.println(answer);
     }
 
-    static int[] solution(int n) {
-        int[] arr = new int[n];
-        arr[0] = 1;
-        arr[1] = 1;
-        for (int i = 2; i < n; i++) {
-            arr[i] = arr[i - 2] + arr[i - 1];
+    static int solution(int n) {
+        int answer = 0;
+
+        boolean[] primeNumbers = new boolean[n + 1];
+        Arrays.fill(primeNumbers, true);
+        primeNumbers[0] = false;
+        primeNumbers[1] = false;
+
+        for (int i = 2; (i * i) <= n; i++) {
+            if (primeNumbers[i]) {
+                for (int j = (i * i); j <= n; j += i) {
+                    primeNumbers[j] = false;
+                }
+            }
         }
-        return arr;
+
+        for (boolean primeNumber : primeNumbers) {
+            if (primeNumber)
+                answer++;
+        }
+
+        return answer;
     }
 
 }
---
+++ src/algorithm/section/array/소수_에라토스테네스_체_05.java
@@ -0,0 +1,5 @@
+package algorithm.section.array;
+
+public class 소수_에라토스테네스_체_05 {
+
+}

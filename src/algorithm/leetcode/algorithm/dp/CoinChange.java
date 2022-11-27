726f705e3856f1662a2854d4468d4d64eafa94ba
kimmin <kimmin2301@gmail.com>
2022-11-27 20:00:10 +0900

    leetcode: CoinChange

--- src/algorithm/leetcode/algorithm/dp/CoinChange.java
+++ src/algorithm/leetcode/algorithm/dp/CoinChange.java
@@ -2,8 +2,43 @@ package algorithm.leetcode.algorithm.dp;
 
 public class CoinChange {
 
-    public int coinChange(int[] coins, int amount) {
+    public static void main(String[] args) {
+        CoinChange s = new CoinChange();
+        int answer = s.coinChange(new int[]{2}, 1);
+        System.out.println("answer = " + answer);
 
-        return 0;
+        CoinChange s1 = new CoinChange();
+        int answer1 = s1.coinChange(new int[]{1, 2, 5}, 11);
+        System.out.println("answer1 = " + answer1);
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int[] m = new int[amount + 1];
+        m[0] = 0;
+
+        for (int i = 1; i < m.length; i++) {
+            int minimumCoin = getMinimumCoin(coins, m, i);
+            if (minimumCoin < 0) {
+                m[i] = -1;
+                continue;
+            }
+            m[i] = minimumCoin + 1;
+        }
+
+        return m[amount];
+    }
+
+    private int getMinimumCoin(int[] coins, int[] m, int n) {
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            // 1 - 2 >= 0 ? false
+            // memoryValue = -1
+            // min = 21억
+            int memoryValue = n - coin >= 0 ? m[n - coin] : -1;
+            if (memoryValue < min && memoryValue >= 0) {
+                min = memoryValue;
+            }
+        }
+        return min != Integer.MAX_VALUE ? min : -1;
     }
 }

671c4c21431e88e40e42830b67ba66231ab73403
kimmin <kimmin2301@gmail.com>
2022-12-29 18:18:32 +0900

    leetcode: FindTheIndexOfTheFirstOccurrenceInAString

---
+++ src/algorithm/leetcode/algorithm/string/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,21 @@
+package algorithm.leetcode.algorithm.string;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    public static void main(String[] args) {
+        int answer = strStr("sadbutsad", "sad");
+        System.out.println("answer = " + answer);
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int needleHash = needle.hashCode();
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            String substring = haystack.substring(i, i + needle.length());
+            if (substring.hashCode() == needleHash && needle.equals(substring)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}

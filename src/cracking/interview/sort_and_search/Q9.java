44a35048a54760b4c20ef90ac2ff0776dfd2a914
kimmin <kimmin2301@gmail.com>
2023-01-06 20:28:24 +0900

    cracking: search Q9

---
+++ src/cracking/interview/sort_and_search/Q9.java
@@ -0,0 +1,29 @@
+package cracking.interview.sort_and_search;
+
+public class Q9 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        boolean isFind = findElement(matrix, 10);
+        System.out.println("isFind = " + isFind);
+    }
+
+    static boolean findElement(int[][] matrix, int elem) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == elem) {
+                return true;
+            } else if (matrix[row][col] > elem) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
